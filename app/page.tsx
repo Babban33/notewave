c01945de9bcb4d8a21bb277329386954ad89935c
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-16 15:06:13 +0530

    added description

--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,11 @@ function HeroSection() {
 
 function CTASection(){
     return(
-        <section className="w-full py-12 md:py-24 lg:py-32 bg-primary dark:bg-primary-foreground">
-            <h2 className="text-3xl text-center font-bold tracking-tighter sm:text-5xl text-primary-foreground dark:text-primary">Ready to Revolutionize Your Note-Taking?</h2>
+        <section className="w-full text-center py-12 md:py-24 lg:py-32 bg-primary dark:bg-primary-foreground">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-primary-foreground dark:text-primary">Ready to Revolutionize Your Note-Taking?</h2>
+            <p className="mx-auto max-w-[700px] md:text-xl text-primary-foreground/80 dark:text-primary/80">
+                Join thousands of users who are already boosting their productivity with Notwave. Start your journey today!
+            </p>
         </section>
     )
 }
