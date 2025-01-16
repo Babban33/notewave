a68212538e3802a9d6133a8c967f22dc634a6e15
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-16 21:11:38 +0530

    added form schema

--- app/(auth-pages)/register/page.tsx
+++ app/(auth-pages)/register/page.tsx
@@ -1,4 +1,38 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import * as z from "zod";
+
+const formSchema = z.object({
+    username: z
+        .string()
+        .min(2, { message: "Username must be at least 2 characters." })
+        .max(20, { message: "Username must not exceed 20 characters." })
+        .regex(/^[a-zA-Z0-9_]+$/, {
+            message: "Username can only contain letters, numbers, and underscores.",
+        }),
+    email: z.string().email({
+        message: "Please enter a valid email address.",
+    }),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters." })
+        .max(30, { message: "Password must not exceed 30 characters." })
+        .regex(/[A-Z]/, {
+            message: "Password must contain at least one uppercase letter.",
+        })
+        .regex(/[a-z]/, {
+            message: "Password must contain at least one lowercase letter.",
+        })
+        .regex(/[0-9]/, {
+            message: "Password must contain at least one number.",
+        })
+        .regex(/[@$!%*?&#]/, {
+            message: "Password must contain at least one special character.",
+        }),
+        confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match.",
+    path: ["confirmPassword"],
+});
 
 export default function RegisterPage(){
     return(
