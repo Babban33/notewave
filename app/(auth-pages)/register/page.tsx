b8714da1c44ea5d39d2feb2cfa9a8879ae305c7f
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-26 18:50:28 +0530

    changed separator and added login link

--- app/(auth-pages)/register/page.tsx
+++ app/(auth-pages)/register/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
@@ -8,8 +8,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { EyeOff, Eye } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
 import { FaGoogle, FaMicrosoft, FaGithub } from 'react-icons/fa';
+import Link from "next/link";
 
 const formSchema = z.object({
     username: z
@@ -63,7 +63,7 @@ export default function RegisterPage(){
 
     return(
         <div className="flex items-center justify-center min-h-screen px-2 md:px-0">
-            <Card className="w-full max-w-lg shadow-lg">
+            <Card className="w-full max-w-md shadow-lg">
                 <CardHeader className="text-center">
                     <CardTitle className="text-xl font-semibold">Register</CardTitle>
                     <CardDescription>Create a new account</CardDescription>
@@ -80,9 +80,8 @@ export default function RegisterPage(){
                             <FaGithub/>
                         </Button>
                     </div>
-                    <div className="relative mb-6">
-                        <Separator />
-                        <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                        <span className="relative z-10 bg-background px-2 text-muted-foreground">
                         Or continue with
                         </span>
                     </div>
@@ -163,6 +162,14 @@ export default function RegisterPage(){
                         </form>
                     </Form>
                 </CardContent>
+                <CardFooter className="justify-center">
+                    <div className="text-center text-sm">
+                        Already have an account?{" "}
+                        <Link href="/login" className="underline underline-offset-4">
+                            Log In
+                        </Link>
+                    </div>
+                </CardFooter>
             </Card>
         </div>
     )
