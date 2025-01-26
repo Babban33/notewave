297e82d9254619dff68c61c1158f593e99019d47
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-01-26 21:02:22 +0530

    made the Alerts absolute and added animation

--- app/(auth-pages)/register/page.tsx
+++ app/(auth-pages)/register/page.tsx
@@ -7,10 +7,11 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { EyeOff, Eye, CheckCircle2, AlertCircle } from "lucide-react";
+import { EyeOff, Eye, CheckCircle2, AlertCircle, X } from "lucide-react";
 import { FaGoogle, FaMicrosoft, FaGithub } from 'react-icons/fa';
 import Link from "next/link";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {motion, AnimatePresence} from "framer-motion"
 
 const formSchema = z.object({
     username: z
@@ -74,132 +75,171 @@ export default function RegisterPage(){
         }
     }
 
+    const closeAlert = () => {
+        setRegistrationSuccess(false)
+        setRegistrationError(null)
+      }
+
     return(
         <div className="flex items-center justify-center min-h-screen px-2 md:px-0">
-            <Card className="w-full max-w-md shadow-lg">
-                <CardHeader className="text-center">
-                    <CardTitle className="text-xl font-semibold">Register</CardTitle>
-                    <CardDescription>Create a new account</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    {registrationSuccess && (
-                        <Alert className="mb-4">
-                            <CheckCircle2 className="h-4 w-4" />
-                            <AlertTitle>Registration Successful!</AlertTitle>
-                            <AlertDescription>
-                                A confirmation email has been sent to your email address. Please check your inbox.
-                            </AlertDescription>
-                        </Alert>
-                        )}
-                        {registrationError && (
-                        <Alert variant="destructive" className="mb-4">
-                            <AlertCircle className="h-4 w-4" />
-                            <AlertTitle>Error</AlertTitle>
-                            <AlertDescription>{registrationError}</AlertDescription>
-                        </Alert>
+            <div className="w-full max-w-md">
+                <AnimatePresence>
+                    {(registrationSuccess || registrationError) && (
+                        <motion.div
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -50 }}
+                            transition={{ duration: 0.3 }}
+                            className="fixed top-16 transform -translate-x-1/2 z-50 w-full max-w-md"
+                        >
+                            <Alert
+                                className={`relative shadow-md ${
+                                registrationSuccess
+                                    ? "bg-green-50 dark:bg-green-900 border-green-200 dark:border-green-800"
+                                    : "bg-destructive/15 dark:bg-destructive border-destructive/50 dark:border-destructive"
+                                }`}
+                            >
+                                <Button className="absolute right-2 top-2" variant="ghost" size="icon" onClick={closeAlert}>
+                                    <X className="h-4 w-4" />
+                                </Button>
+                                {registrationSuccess ? (
+                                    <CheckCircle2 className="h-5 w-5 text-green-600 dark:text-green-400" />
+                                    ) : (
+                                    <AlertCircle className="h-5 w-5 text-destructive dark:text-destructive-foreground" />
+                                )}
+                                <AlertTitle
+                                    className={`text-lg font-semibold ${
+                                        registrationSuccess
+                                        ? "text-green-800 dark:text-green-200"
+                                        : "text-destructive dark:text-destructive-foreground"
+                                    }`}
+                                >
+                                    {registrationSuccess ? "Registration Successful!" : "Registration Error"}
+                                </AlertTitle>
+                                <AlertDescription
+                                    className={`mt-1 text-sm ${
+                                        registrationSuccess
+                                        ? "text-green-700 dark:text-green-300"
+                                        : "text-destructive dark:text-destructive-foreground"
+                                    }`}
+                                >
+                                    {registrationSuccess
+                                    ? "A confirmation email has been sent. Please check your inbox to complete your registration."
+                                    : registrationError}
+                                </AlertDescription>
+                            </Alert>
+                        </motion.div>
                     )}
-                    <div className="mb-6 gap-4 grid grid-cols-3">
-                        <Button variant="outline">
-                            <FaGoogle/>
-                        </Button>
-                        <Button variant="outline">
-                            <FaMicrosoft/>
-                        </Button>
-                        <Button variant="outline">
-                            <FaGithub/>
-                        </Button>
-                    </div>
-                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                        <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                        Or continue with
-                        </span>
-                    </div>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                            <FormField
-                                name="username"
-                                render={({field})=>(
+                </AnimatePresence>
+                <Card className="w-full max-w-md shadow-lg">
+                    <CardHeader className="text-center">
+                        <CardTitle className="text-xl font-semibold">Register</CardTitle>
+                        <CardDescription>Create a new account</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="mb-6 gap-4 grid grid-cols-3">
+                            <Button variant="outline">
+                                <FaGoogle/>
+                            </Button>
+                            <Button variant="outline">
+                                <FaMicrosoft/>
+                            </Button>
+                            <Button variant="outline">
+                                <FaGithub/>
+                            </Button>
+                        </div>
+                        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                            Or continue with
+                            </span>
+                        </div>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                                <FormField
+                                    name="username"
+                                    render={({field})=>(
+                                        <FormItem>
+                                            <FormLabel className="text-primary"> Username </FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="John Doe" {...field} autoComplete="name"/>
+                                            </FormControl>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel className="text-primary">Email</FormLabel>
+                                            <FormControl>
+                                                <Input type="email" placeholder="john@example.com" {...field} autoComplete="email"/>
+                                            </FormControl>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel className="text-primary">Password</FormLabel>
+                                            <FormControl>
+                                                <div className="relative">
+                                                    <Input 
+                                                        type={showPassword ? "text" : "password"} 
+                                                        placeholder="********" 
+                                                        {...field} 
+                                                        autoComplete="new-password"
+                                                    />
+                                                    <Button
+                                                        type="button"
+                                                        variant="ghost"
+                                                        size="sm"
+                                                        className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                                                        onClick={() => setShowPassword(!showPassword)}
+                                                    >
+                                                        {showPassword ? (
+                                                            <EyeOff className="h-4 w-4" />
+                                                        ) : (
+                                                            <Eye className="h-4 w-4" />
+                                                        )}
+                                                    </Button>
+                                                </div>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="confirmPassword"
+                                    render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel className="text-primary"> Username </FormLabel>
+                                        <FormLabel>Confirm Password</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="John Doe" {...field} autoComplete="name"/>
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="text-primary">Email</FormLabel>
-                                        <FormControl>
-                                            <Input type="email" placeholder="john@example.com" {...field} autoComplete="email"/>
-                                        </FormControl>
-                                        <FormMessage/>
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="text-primary">Password</FormLabel>
-                                        <FormControl>
-                                            <div className="relative">
-                                                <Input 
-                                                    type={showPassword ? "text" : "password"} 
-                                                    placeholder="********" 
-                                                    {...field} 
-                                                    autoComplete="new-password"
-                                                />
-                                                <Button
-                                                    type="button"
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                                                    onClick={() => setShowPassword(!showPassword)}
-                                                >
-                                                    {showPassword ? (
-                                                        <EyeOff className="h-4 w-4" />
-                                                    ) : (
-                                                        <Eye className="h-4 w-4" />
-                                                    )}
-                                                </Button>
-                                            </div>
+                                            <Input type="password" placeholder="********" {...field} autoComplete="on"/>
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="confirmPassword"
-                                render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Confirm Password</FormLabel>
-                                    <FormControl>
-                                        <Input type="password" placeholder="********" {...field} autoComplete="on"/>
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                                )}
-                            />
-                            <Button type="submit" className="w-full">Register</Button>
-                        </form>
-                    </Form>
-                </CardContent>
-                <CardFooter className="justify-center">
-                    <div className="text-center text-sm">
-                        Already have an account?{" "}
-                        <Link href="/login" className="underline underline-offset-4">
-                            Log In
-                        </Link>
-                    </div>
-                </CardFooter>
-            </Card>
+                                    )}
+                                />
+                                <Button type="submit" className="w-full">Register</Button>
+                            </form>
+                        </Form>
+                    </CardContent>
+                    <CardFooter className="justify-center">
+                        <div className="text-center text-sm">
+                            Already have an account?{" "}
+                            <Link href="/login" className="underline underline-offset-4">
+                                Log In
+                            </Link>
+                        </div>
+                    </CardFooter>
+                </Card>
+            </div>
         </div>
     )
 }
