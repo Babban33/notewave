9cd6248058a76926ec45e06c7b88b908d0e9e4d5
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2025-02-04 16:28:14 +0530

    added avatar and updated navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,38 +1,48 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription } from "@/components/ui/sheet"
-import { MoonIcon, SunIcon, MenuIcon } from 'lucide-react'
-import { useTheme } from "next-themes"
-
-const navItems = [
-    { name: "Login", path: "/login", type: "ghost"},
-    { name: "Register", path:"/register", type: "default"}
-]
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { MoonIcon, SunIcon, MenuIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Session } from "@supabase/supabase-js";
+import { signout } from "@/app/actions";
 
 export default function Navbar() {
-    const { theme, setTheme } = useTheme()
+    const { theme, setTheme } = useTheme();
+    const [session, setSession] = useState<Session | null>(null);
+
+    // Check user authentication state
+    useEffect(() => {
+        const supabase = createClient();
+        const checkUser = async () => {
+            const { data: { session } } = await supabase.auth.getSession();
+            setSession(session);
+        };
+
+        checkUser();
+
+        // Listen for authentication state changes
+        const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session);
+        });
+
+            return () => {
+            authListener.subscription.unsubscribe();
+            };
+    }, []);
 
     return (
         <nav className="sticky top-0 z-40 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-b border-border">
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
-                    <div className="flex items-center">
-                        <Link href="/" className="flex items-center space-x-2">
+                    <Link href="/" className="flex items-center space-x-2">
                         <span className="font-bold text-xl">📝 Notewave</span>
-                        </Link>
-                    </div>
+                    </Link>
                     <div className="flex items-center space-x-4">
-                        <div className="hidden sm:flex space-x-4">
-                            {navItems.map((item)=>(
-                                <Link key={item.name} href={item.path}>
-                                    <Button variant={item.type as "default" | "outline" | "link" | "destructive" | "secondary" | "ghost"}>
-                                        {item.name}
-                                    </Button>
-                                </Link>
-                            ))}
-                        </div>
                         <Button
                             variant="ghost"
                             size="icon"
@@ -44,6 +54,28 @@ export default function Navbar() {
                             <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                             <span className="sr-only">Toggle theme</span>
                         </Button>
+
+                        {session ? (
+                            <div className="flex items-center space-x-4">
+                                <Avatar>
+                                    <AvatarImage src={session.user.user_metadata?.avatar_url || ""} alt="User Avatar" />
+                                    <AvatarFallback>{session.user.email?.charAt(0).toUpperCase()}</AvatarFallback>
+                                </Avatar>
+                                <Button onClick={signout} className="hidden md:visible">
+                                    Sign Out
+                                </Button>
+                            </div>
+                        ) : (
+                            <div className="hidden sm:flex space-x-4">
+                                <Link href="/login">
+                                    <Button variant="ghost">Login</Button>
+                                </Link>
+                                <Link href="/register">
+                                    <Button variant="default">Register</Button>
+                                </Link>
+                            </div>
+                        )}
+                        
                         <Sheet>
                             <SheetTrigger asChild>
                                 <Button variant="ghost" size="icon" className="sm:hidden rounded-full">
@@ -52,19 +84,25 @@ export default function Navbar() {
                                 </Button>
                             </SheetTrigger>
                             <SheetContent side="right">
-                                <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
-                                <SheetDescription>Notewave</SheetDescription>
                                 <nav className="flex flex-col mt-4 space-y-4">
-                                    {navItems.map((item) => (
-                                        <Link key={item.path} href={item.path} className="w-full">
-                                            <Button
-                                                variant={item.type as "default" | "outline" | "link" | "destructive" | "secondary" | "ghost"}
-                                                className="w-full py-2 text-center"
-                                            >
-                                                {item.name}
-                                            </Button>
-                                        </Link>
-                                    ))}
+                                    {session ? (
+                                        <Button variant="destructive" onClick={signout} className="w-full">
+                                        S   ign Out
+                                        </Button>
+                                    ) : (
+                                        <>
+                                            <Link href="/login">
+                                                <Button variant="ghost" className="w-full">
+                                                    Login
+                                                </Button>
+                                            </Link>
+                                            <Link href="/register">
+                                                <Button variant="default" className="w-full">
+                                                    Register
+                                                </Button>
+                                            </Link>
+                                        </>
+                                    )}
                                 </nav>
                             </SheetContent>
                         </Sheet>
@@ -72,5 +110,5 @@ export default function Navbar() {
                 </div>
             </div>
         </nav>
-    )
+    );
 }
